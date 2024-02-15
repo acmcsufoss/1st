b1b3a38cf99c26f90738b043468fd617fdf0b304
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-02-15 15:14:38 -0800

    add favicon to html

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ echo "<!DOCTYPE html>
   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
 
   <link rel=\"stylesheet\" href=\"style.css\" />
+  <link rel=\"icon\" href=\"favicon.ico\" />
   <title>1st | acmcsufoss</title>
 </head>
 <body>
